2b0946ef91a5743d668b495eaabe017e594e9fd8
Ravi Ranjan <ravi2411rj@gmail.com>
2025-06-01 18:37:40 +0530

    Initial commit: Spring Boot Product Catalogue Service

---
+++ src/main/java/com/ecommerce/productcatalogue/ProductCatalogueServiceApplication.java
@@ -0,0 +1,15 @@
+package com.ecommerce.productcatalogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCatalogueServiceApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ProductCatalogueServiceApplication.class, args);
+
+	}
+
+}
